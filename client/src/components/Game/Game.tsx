5aaee97c816ab1e3aac58a0af4db45238b9020cd
FloweyAndinov <flowey.andinov@gmail.com>
2024-02-10 22:20:34 +0200

    Feature : syllable holder

--- client/src/components/Game/Game.tsx
+++ client/src/components/Game/Game.tsx
@@ -65,8 +65,14 @@ function Game({socket , isOwner, roomIDProp} : Props) {
     <>
     <div>Game</div>
     <div className={styles.container}>
-    <div style={{position:'absolute', width:'100px', height:'100px', backgroundColor:'green', left:'-30%', top:'-100%'}}></div>
-    <span style={{color:'transparent'}}>Player 0</span>
+
+    <div style={{position:'absolute', width:'max-content', height:'100px', backgroundColor:'green', left:'-120%', top:'-100%'}}>
+      <span style={{width: 'max-content'}}>Write a word that cointains</span>
+      <br />
+      <span style={{color: 'red'}}>{word}</span>
+    </div>
+    
+    <span style={{color:'transparent'}}>Player 0</span> {/*helps with positioning the image */}
         <div className={styles.circle_deg_0}>
         Player 1
       </div> 
@@ -100,6 +106,7 @@ function Game({socket , isOwner, roomIDProp} : Props) {
       </div> 
       
     </div>
+    
     
     <div className={styles.textsend}>
       <WordSection socket={socket} enabled={playType} word={word} playerword={playerWord} />
