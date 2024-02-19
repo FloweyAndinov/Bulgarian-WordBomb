30bb8f1e8f1052cfaf6db6efb51f99b3d9eddcd2
FloweyAndinov <flowey.andinov@gmail.com>
2024-02-19 09:22:38 +0200

    Feature : invite links (reciever)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,13 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { BrowserRouter } from 'react-router-dom';
 import './App.css'
-import {Socket, io} from 'socket.io-client'
 import Home from './components/Home/Home';
 import {socket} from './socket'
 function App() {
-  const [isConnected, setIsConnected] = useState(socket.connected);
-
-
- 
-
 
   return (
-    <>
+    <BrowserRouter>
     {socket ? <Home socket={socket}/> : null}
-    </>
+    </BrowserRouter>
   )
 }
 
--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,9 +1,11 @@
 
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import styles from './Home.module.scss'
 import Lobby from '../Lobby/Lobby';
 import Join from '../Join/Join';
 import { Socket } from 'socket.io-client';
+import Game from '../Game/Game';
 
 interface Props {
     socket: Socket;
@@ -11,14 +13,23 @@ interface Props {
 
 function Home( {socket}: Props) {
 
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [query, setQuery] = useState(searchParams.get('id'))
     const [showJoin, setShowJoin] = useState(false);
     const [showCreate, setShowCreate] = useState(false);
+    const [showJoinInvite, setShowJoinInvite] = useState(false);
     const [nameText, setNameText] = useState('');
     
     useEffect(() => {
+        console.log(query)
+
         socket.on('connect', () => {
-        // console.log(socket.id);
-        });
+            
+            if (query?.trim()!=null) {
+                console.log("query is not null!!")
+                setShowJoinInvite(true)
+            }
+        });      
     }, [socket]);
 
     const handleInputChange = (e : React.ChangeEvent<HTMLInputElement>) => {
@@ -33,11 +44,19 @@ function Home( {socket}: Props) {
         return <Join />;
     }
 
+    if (showJoinInvite) {
+        if (query) {
+        socket.emit('join-room-window', query);
+        return <Lobby socket={socket} isOwner={false} roomIDProp={query}/>
+        }
+    }
+
     if (showCreate) {
         return <Lobby socket={socket} isOwner={true}/>
     }
   return (
     <>
+    
     <div className={styles.title}>
         WordBomb на български
     </div>
@@ -50,7 +69,9 @@ function Home( {socket}: Props) {
         <button onClick={() => setShowCreate(true)}>Create</button>
         <button onClick={() => setShowJoin(true)}>Join</button>
     </div>
+    
     </>
+    
   )
 }
 
